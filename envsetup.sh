06bd2601ce109e2c7a6ecf17b0e58b76e1a7b953
Dragon <dragon7780@hotmail.nl>
2014-11-16 13:25:52 +0100

    This has to be fusion
    Signed-off-by: Dragon <dragon7780@hotmail.nl>

--- envsetup.sh
+++ envsetup.sh
@@ -536,7 +536,7 @@ function breakfast()
     FUSION_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
-    for f in `/bin/ls vendor/cm/vendorsetup.sh 2> /dev/null`
+    for f in `/bin/ls vendor/fusion/vendorsetup.sh 2> /dev/null`
         do
             echo "including $f"
             . $f
@@ -2293,7 +2293,7 @@ unset f
 
 # Add completions
 check_bash_version && {
-    dirs="sdk/bash_completion vendor/cm/bash_completion"
+    dirs="sdk/bash_completion vendor/fusion/bash_completion"
     for dir in $dirs; do
     if [ -d ${dir} ]; then
         for f in `/bin/ls ${dir}/[a-z]*.bash 2> /dev/null`; do
